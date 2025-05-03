d5343c1f010ad0b595280f0ab27ba18bee2f92dc
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-03 10:12:17 +0545

    string, boolean & uint

--- 02variables/main.go
+++ 02variables/main.go
@@ -5,5 +5,13 @@ import "fmt"
 func main() {
 	var username string = "I am learning golang"
 	fmt.Println(username) //Println doesn't support formatting like %T
-	fmt.Printf("Type of variable: %T", username)
+	fmt.Printf("Type of variable: %T\n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Type of variable: %T\n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Type of variable: %T\n", smallVal)
 }
