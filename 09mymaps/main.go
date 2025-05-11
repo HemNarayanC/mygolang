984ad470629e415caed43855be0e76285327059e
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-11 19:55:18 +0545

    map syntax, declaration, initialization and adding items in map

---
+++ 09mymaps/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*maps is used to store key-value pairs
+	Syntax:
+			make(map[type of key]type of value)
+	*/
+
+	currencyCode := make(map[string]string)
+	fmt.Println(currencyCode)
+
+	//adding items to the map
+	currencyCode["USD"] = "US Dollar"
+	currencyCode["NPR"] = "Nepalese Rupee"
+	currencyCode["EUR"] = "Euro"
+	currencyCode["QAR"] = "Qatari Riyal"
+	fmt.Println(currencyCode)
+
+}
