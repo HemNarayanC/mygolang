18525d5ec8cef86b5655985f85c67122f06fc536
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-14 19:39:58 +0545

    methods in go

---
+++ 15mymethods/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	user1 := User{
+		Name:   "Hemant",
+		Email:  "hemant@gmail.com",
+		Age:    23,
+		status: true,
+	}
+
+	fmt.Println(user1)
+	user1.NewMail()
+	fmt.Printf("Details of user1 are: %+v\n", user1)
+
+	//calculating area of circle
+	circle := Circle{
+		Radius: 5,
+	}
+	circleArea := circle.Area()
+	fmt.Printf("Area of circle is: %v\n", circleArea)
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Age    int
+	status bool
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Rectangle struct {
+	Length  int
+	Breadth int
+}
+
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
+}
+
+func (u User) NewMail() {
+	u.Email = "newmail@gmail.com"
+	fmt.Println("New email is: ", u.Email)
+}
