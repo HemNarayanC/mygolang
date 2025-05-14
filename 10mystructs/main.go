a070fe2d53ce0c07bff31c833b0bf0e2effc0c60
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-14 08:22:39 +0545

    accessing the field in struct

--- 10mystructs/main.go
+++ 10mystructs/main.go
@@ -12,4 +12,9 @@ type User struct {
 func main() {
 	User1 := User{"Hemant", "hemant@gmail.com", 23, true}
 	fmt.Println(User1)
+
+	fmt.Printf("Details of User1 are: %+v\n", User1)
+	//accessing the field
+	fmt.Println(User1.Name)
+	fmt.Printf("Name: %v\nEmail: %v\nAge: %v\n", User1.Name, User1.Email, User1.Age)
 }
