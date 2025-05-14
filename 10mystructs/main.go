46b7eac3959880ca918a0064c9bca49f414e80f0
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-14 08:25:29 +0545

    creating struct specifying field names

--- 10mystructs/main.go
+++ 10mystructs/main.go
@@ -13,8 +13,17 @@ func main() {
 	User1 := User{"Hemant", "hemant@gmail.com", 23, true}
 	fmt.Println(User1)
 
+	User2 := User{
+		Name:   "Sudip",
+		Email:  "sudip@gmail.com",
+		Age:    25,
+		status: false,
+	}
+
 	fmt.Printf("Details of User1 are: %+v\n", User1)
 	//accessing the field
 	fmt.Println(User1.Name)
 	fmt.Printf("Name: %v\nEmail: %v\nAge: %v\n", User1.Name, User1.Email, User1.Age)
+
+	fmt.Printf("Details of User2 are: %+v\n", User2)
 }
