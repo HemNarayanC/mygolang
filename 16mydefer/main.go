bbac562f3123800b1c91b78406cd4ae0be9c845d
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-15 06:02:11 +0545

    defer with function

--- 16mydefer/main.go
+++ 16mydefer/main.go
@@ -6,10 +6,18 @@ func main() {
 	defer fmt.Println("\nWorld")
 	fmt.Println("Hello ")
 	myDefer()
+
+	a := 199
+	defer displayValue(a)
+
 }
 
 func myDefer() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Print(i)
 	}
+}
+
+func displayValue(val int) {
+	fmt.Println("\nValue is: ", val)
 }
