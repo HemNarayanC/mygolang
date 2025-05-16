28348bb6c298d2e5110087025f86ec35ef11cbe8
Hem Narayan Chaudhary <hemnarayan.chaudhary.insight@gmail.com>
2025-05-16 17:25:39 +0545

    pointers to struct

--- 10mystructs/main.go
+++ 10mystructs/main.go
@@ -10,20 +10,30 @@ type User struct {
 }
 
 func main() {
-	User1 := User{"Hemant", "hemant@gmail.com", 23, true}
-	fmt.Println(User1)
+	user1 := User{"Hemant", "hemant@gmail.com", 23, true}
+	fmt.Println(user1)
 
-	User2 := User{
+	//creating struct specifying field names
+	user2 := User{
 		Name:   "Sudip",
 		Email:  "sudip@gmail.com",
 		Age:    25,
 		status: false,
 	}
 
-	fmt.Printf("Details of User1 are: %+v\n", User1)
-	//accessing the field
-	fmt.Println(User1.Name)
-	fmt.Printf("Name: %v\nEmail: %v\nAge: %v\n", User1.Name, User1.Email, User1.Age)
+	user3 := &User{
+		Name:   "Omrina Chaudhary",
+		Email:  "omrina200@gmail.com",
+		Age:    13,
+		status: true,
+	}
 
-	fmt.Printf("Details of User2 are: %+v\n", User2)
+	fmt.Printf("Details of User1 are: %+v\n", user1)
+	//accessing the field
+	fmt.Println(user1.Name)
+	fmt.Printf("Name: %v\nEmail: %v\nAge: %v\n", user1.Name, user1.Email, user1.Age)
+
+	fmt.Printf("Details of User2 are: %+v\n", user2)
+
+	fmt.Printf("Name of user3 is %v", (*user3).Name)
 }
